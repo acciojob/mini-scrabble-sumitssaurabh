3397d177215baf05ef7d883b11fc270858d81662
sumitssaurabh <117895961+sumitssaurabh@users.noreply.github.com>
2023-01-15 21:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+function countChar(countFrom,displayout){
+	var length = document.getElementById(countFrom).value.length;
+	document.getElementById(displayout).innerHTML = length;
+}
